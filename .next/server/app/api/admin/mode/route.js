1ced6f474be04a4be3db39c92555e469fa341fb9
Z User <z@container>
2026-08-07 20:33:58 +0000

    f0ad7bfb-5c98-46cf-82f9-f9c767cdbd9d

---
+++ .next/server/app/api/admin/mode/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/admin/mode/route.js")
+R.c("server/chunks/[root-of-the-server]__c9f270d5._.js")
+R.c("server/chunks/[root-of-the-server]__d69c7275._.js")
+R.c("server/chunks/_next-internal_server_app_api_admin_mode_route_actions_c097695e.js")
+R.m(86503)
+module.exports=R.m(86503).exports
